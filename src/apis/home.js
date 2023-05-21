c4e4a0a3a60b34c3921473ecd431f347886eb4a1
-程序媛小艾 <AILIN_GT_13@163.com>
2023-05-21 17:41:03 +0800

    home-product列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,10 +1,10 @@
-import httpInstance from '@/utils/http'
+import httpInstance from "@/utils/http";
 
 // 获取banner
-export function getBannerAPI(){
-    return httpInstance({
-        url:"/home/banner"
-    })
+export function getBannerAPI() {
+  return httpInstance({
+    url: "/home/banner",
+  });
 }
 
 /**
@@ -13,10 +13,10 @@ export function getBannerAPI(){
  * @return {*}
  */
 export const findNewAPI = () => {
-    return httpInstance({
-      url:'/home/new'
-    })
-}
+  return httpInstance({
+    url: "/home/new",
+  });
+};
 
 /**
  * @description: 获取人气推荐
@@ -24,9 +24,18 @@ export const findNewAPI = () => {
  * @return {*}
  */
 export const getHotAPI = () => {
-    return  httpInstance(
-        {
-            url:'home/hot'
-        }
-    )
-  }
+  return httpInstance({
+    url: "home/hot",
+  });
+};
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: "/home/goods",
+  });
+};
