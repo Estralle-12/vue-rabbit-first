c01096242c79ea14741e92071beff550a1d30607
-程序媛小艾 <AILIN_GT_13@163.com>
2023-05-29 17:34:37 +0800

    接口购物车-加入购物车实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,24 +1,36 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
+import { useUserStore } from "./user";
+import { insertCartAPI, findNewCartListAPI } from "@/apis/cart";
 
 export const useCartStore = defineStore(
   "cart",
   () => {
+    const userStore = useUserStore();
+    const isLogin = computed(() => userStore.userInfo.token);
     // 定义state
     const cartList = ref([]);
     // 定义action-addCart
-    const addCart = (goods) => {
-      // 添加购物车操作
-      // 已添加过 - count+1
-      // 没有添加过 -直接push
-      // 思路：通过匹配传递过来的商品对象中的skuId能不能再cartList中找到，找到了就是添加过
-      const item = cartList.value.find((item) => goods.skuId === item.skuId);
-      if (item) {
-        // 找到了
-        item.count++;
+    const addCart = async (goods) => {
+      const { skuId, count } = goods;
+      if (isLogin.value) {
+        // 登录之后的加入购物车逻辑
+        await insertCartAPI({ skuId, count });
+        const res = await findNewCartListAPI();
+        cartList.value = res.value;
       } else {
-        // 没找到
-        cartList.value.push(goods);
+        // 添加购物车操作
+        // 已添加过 - count+1
+        // 没有添加过 -直接push
+        // 思路：通过匹配传递过来的商品对象中的skuId能不能再cartList中找到，找到了就是添加过
+        const item = cartList.value.find((item) => goods.skuId === item.skuId);
+        if (item) {
+          // 找到了
+          item.count++;
+        } else {
+          // 没找到
+          cartList.value.push(goods);
+        }
       }
     };
 
@@ -35,18 +47,29 @@ export const useCartStore = defineStore(
     };
 
     // 全选功能
-    const addCheck = (selected) => {
-      cartList.value.forEach((item) => item.selected === selected);
+    const allCheck = (selected) => {
+      cartList.value.forEach((item) => (item.selected = selected));
     };
     // 计算属性
     const allCount = computed(() =>
       cartList.value.reduce((a, c) => a + c.count, 0)
     );
-
+    // 总价
     const allPrice = computed(() =>
       cartList.value.reduce((a, c) => a + c.count * c.price, 0)
     );
-
+    // 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count, 0)
+    );
+    // 已选择商品价钱合计
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count * c.price, 0)
+    );
     // 是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected));
     return {
@@ -57,7 +80,9 @@ export const useCartStore = defineStore(
       allPrice,
       singleCheck,
       isAll,
-      addCheck,
+      allCheck,
+      selectedCount,
+      selectedPrice,
     };
   },
   {
